beb4fd917eafd664105041cc02a004890216f4bb
sandeep6153 <sandeep.4373@gmail.com>
2017-05-26 12:26:50 +0530

    moved client code to separate repo

--- PRM_Client/app/app.articles.ts
+++
@@ -1,8 +0,0 @@
-import {Component,Input} from '@angular/core';
-@Component({
-    selector:'articles',
-    template:'<h1><ng-content></ng-content>{{articleTitle}}</h1>'
-})
-export class ArticlesComponent{
-    @Input() articleTitle:string;
-}
